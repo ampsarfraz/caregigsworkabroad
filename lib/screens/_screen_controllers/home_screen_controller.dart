0821e9d28106e7c0454b797449ff40b7319b7159
saravananravi08 <saravanan.ravi@ampcome.com>
2022-10-13 11:16:18 +0530

    implemented fun for moving app bar

--- lib/screens/_screen_controllers/home_screen_controller.dart
+++ lib/screens/_screen_controllers/home_screen_controller.dart
@@ -1,10 +1,42 @@
 // ignore_for_file: unnecessary_overrides
 
-import 'package:get/get.dart';
+import 'package:caregigsworkabroad/utils/common_imports.dart';
+import 'package:flutter/rendering.dart';
 
 class HomeScreenController extends GetxController {
+  RxDouble hContainer = 100.0.obs;
   RxBool showImage = false.obs;
   RxString imageString = ''.obs;
+  ScrollController feedController = ScrollController();
+  late ScrollDirection direction;
+
+  reduceHeight() {
+    if (hContainer.value > 0) {
+      hContainer(hContainer.value - 5);
+    }
+  }
+
+  increaseHeight() {
+    if (hContainer.value < 100) {
+      hContainer(hContainer.value + 5);
+    }
+  }
+
+  scrollListener() {
+    feedController.addListener(() {
+      //   print(pixels);
+      int pixels = feedController.position.pixels.toInt();
+      if (direction == ScrollDirection.reverse) {
+        reduceHeight();
+      } else if (direction == ScrollDirection.forward) {
+        increaseHeight();
+        if (pixels == 0) {
+          hContainer(100);
+        }
+      }
+    });
+  }
+
   @override
   void onInit() {
     super.onInit();
@@ -12,6 +44,7 @@ class HomeScreenController extends GetxController {
 
   @override
   void onReady() {
+    scrollListener();
     super.onReady();
   }
 
